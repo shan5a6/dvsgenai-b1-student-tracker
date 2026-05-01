5eb7cc58b717bd101360cdafb3a988770133f588
shan5a6 <shahan.aix@gmail.com>
2026-05-01 21:57:55 +0400

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -18,7 +18,7 @@ sns.set_theme(style="whitegrid")
 # CONFIG
 # ================================
 SHEET_ID            = os.environ["SHEET_ID"]
-WORKSHEET_INDEX     = int(os.environ.get("WORKSHEET_INDEX", "5"))
+WORKSHEET_INDEX     = int(os.environ.get("WORKSHEET_INDEX", "3"))
 CREDENTIALS_JSON    = os.environ["GOOGLE_CREDENTIALS_JSON"]
 
 # ================================
